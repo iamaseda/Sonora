e5df00f49128c6c6c3d846811a003b76b75fede3
iamaseda <asedaasomani@gmail.com>
2023-12-18 01:36:42 -0500

    Figured out how to get all songs from Liked Songs automatically without having to hardcode or manually input the number of songs in the Liked Songs collection

--- sonicScraper.py
+++ sonicScraper.py
@@ -2,7 +2,7 @@ import json
 import spotifyEnvironment
 import spotipy.util as util
 import spotipy
-import os
+import re
 
 client_id = spotifyEnvironment.client_id
 client_secret = spotifyEnvironment.client_secret
@@ -20,16 +20,27 @@ token = util.prompt_for_user_token(username,
                                    client_secret=client_secret,
                                    redirect_uri=redirect_uri)
 
-#get songs from Liked Songs
-songs = {}
-i = 0
-if token:
-    while i<2500:
-        sp = spotipy.Spotify(auth=token)
-        results = sp.current_user_saved_tracks(offset=i, limit=50)
-        songs[i] = results
-        i += 50
+def getLikedSongs():
+    #get songs from Liked Songs
+    songs = {}
+    i = 0
+    
+    if token:
+        sp1 = spotipy.Spotify(auth=token)
+        liked_songs = sp1.current_user_saved_tracks()
+        size = liked_songs['total']
+        while i < size:
+            sp = spotipy.Spotify(auth=token)
+            results = sp.current_user_saved_tracks(offset=i, limit=50)
+            songs[i] = results
+            i += 50
 
-#dump to json
-with open('songs.json', 'w') as fp:
-    json.dump(songs, fp)
+    #dump to json
+    with open('songs.json', 'w') as fp:
+        json.dump(songs, fp)
+
+
+
+if __name__ == "__main__":
+
+    getLikedSongs()
