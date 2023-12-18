17acc6186f64d6eab0dbf810620282f32d549102
iamaseda <asedaasomani@gmail.com>
2023-12-18 03:10:52 -0500

    Categorization does not categorize as I'd like. Uses the search categorizations rather than album. Saving before attempting to categorize more finely.

--- sonicScraper.py
+++ sonicScraper.py
@@ -29,15 +29,30 @@ def getLikedSongs():
         sp1 = spotipy.Spotify(auth=token)
         liked_songs = sp1.current_user_saved_tracks()
         size = liked_songs['total']
+        
         while i < size:
             sp = spotipy.Spotify(auth=token)
             results = sp.current_user_saved_tracks(offset=i, limit=50)
             songs[i] = results
             i += 50
+        try:
+            tracks = liked_songs['items']
+            categories = set()
+
+            for song in tracks:
+                category = song['track']['album'].get('category', None)
+                if category:
+                    categories.add(category['name'])
+        except Exception as e:
+            print(f"Error: {e}")
+            return None
 
     #dump to json
     with open('songs.json', 'w') as fp:
         json.dump(songs, fp)
+
+    with open('genres.json', 'w') as fp:
+        json.dump(categories, fp)
 
 def getCatefories():
     genres = {}
