ea7abcb332e26d532b7d30a1c1ee79a04777c552
iamaseda <asedaasomani@gmail.com>
2023-12-19 01:44:33 -0500

    Had to backtrack. Will need to find genres another way. Currently able to get all songs along with their IDs. Next step is attempting to get full paging objects to pull genres from.

--- sonicScraper.py
+++ sonicScraper.py
@@ -2,6 +2,7 @@ import json
 import spotifyEnvironment
 import spotipy.util as util
 import spotipy
+import time
 import re
 
 client_id = spotifyEnvironment.client_id
@@ -23,64 +24,89 @@ token = util.prompt_for_user_token(username,
 def getLikedSongs():
     #get songs from Liked Songs
     songs = {}
+    genres = {}
     i = 0
     
     if token:
         sp1 = spotipy.Spotify(auth=token)
         liked_songs = sp1.current_user_saved_tracks()
         size = liked_songs['total']
-        
+        print("Starting while loop\n")
         while i < size:
             sp = spotipy.Spotify(auth=token)
+            # Results stores songs in clusters of 50
             results = sp.current_user_saved_tracks(offset=i, limit=50)
             songs[i] = results
-            i += 50
-        try:
-            tracks = liked_songs['items']
-            categories = set()
 
-            for song in tracks:
-                category = song['track']['album'].get('category', None)
-                if category:
-                    categories.add(category['name'])
-        except Exception as e:
-            print(f"Error: {e}")
-            return None
+            # print(i+50,"\n")
+            i += 50
+        print("\nSongs finished being parsed\n")
+        # try:
+        #     # Tracks stores all the tracks in the liked songs
+        #     # tracks = liked_songs['items']
+        #     # categories = set()
+        #     # print(tracks)
+        #     for _, song in songs.items():
+        #         # Find the album within which a song can be found
+        #         album = song['track']['album']
+        #         if album:
+        #             # Album information is available. Use its category
+        #             genre = album.get('genres', [])
+        #             # genre = album[0]
+        #             # if category:
+        #             #     categories.add(category['name'])
+        #             print(genre, "\n")
+        #             # if genre not in genres:
+        #             #     genres[genre['name']] = []
+        #             # genres[genre['name']].append(genre['id'])
+        # except Exception as e:
+        #     print(f"Error: {e}")
+        #     return None
 
     #dump to json
     with open('songs.json', 'w') as fp:
         json.dump(songs, fp)
 
-    with open('genres.json', 'w') as fp:
-        json.dump(categories, fp)
 
-def getCatefories():
+def getCategories():
     genres = {}
     holder = []
+    i=0
+    j=0
     if token:
         try:
             sp = spotipy.Spotify(auth=token)
-            categories_response = sp.categories()
-            categories = categories_response['categories']['items']
-            sumCategories = len(categories)
-            for i in range(sumCategories):
-                genre_response = sp.categories(offset=i, limit=1)
-                genre = genre_response['categories']['items'][0]
-                if genre['name'] not in genres:
-                    genres[genre['name']] = []
-                genres[genre['name']].append(genre['id'])
-                # holder.append(genre['name'])
+            liked_songs = sp.current_user_saved_tracks()
+            size = liked_songs['total']
+            print("Starting while loop\n")
+            while i < size:
+                results = sp.current_user_saved_tracks(offset=i, limit=50)
+                i += 50
+                print("\nStarting For loop. Size of results is \n", results['total'] - i)
+                for song in results['items']:
+                    print("\nSong found: ", song['track']['name'])
+                    track_id = song['track']['id']
+                    print("\nTrack ID found: ", track_id)
+                    
+                    # track = sp.track(track_id)
+                    # print("\nTrack found")
+                    # artist = track['artists'][0]
+                    # #artist = sp.artist(track_id)
 
-            #dump to json
-            with open('genres.json', 'w') as fp:
-                json.dump(genres, fp)
+                    # j += 1
+                    # print("\nArtist ",j," : ", artist)
+                    # time.sleep(1)
+            # #dump to json
+            # with open('genres.json', 'w') as fp:
+            #     json.dump(artist, fp)
 
-            return categories
+            
+
         except Exception as e:
             print(f"Error: {e}")
             return None
 
 if __name__ == "__main__":
 
-    #getLikedSongs()
-    getCatefories()
+    # getLikedSongs()
+    getCategories()
